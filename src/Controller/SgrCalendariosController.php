7482ed4f808c212b83f1728d0434373f481253b6
juanantoniofr <juanantonio.fr@gmail.com>
2020-03-11 14:03:55 +0100

    calendarios

--- src/Controller/SgrCalendariosController.php
+++ src/Controller/SgrCalendariosController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use App\Repository\SgrEspacioRepository;
+use App\Repository\SgrTermnoRepository;
+
 class SgrCalendariosController extends AbstractController
 {
     
@@ -12,9 +15,16 @@ class SgrCalendariosController extends AbstractController
     /**
        * @Route("/calendarios.html", name="sgr_calendarios_index")
     */
-    public function index()
+    public function index(SgrEspacioRepository $sgrEspacioRepository, SgrTerminoRepository $sgrTerminoRepository)
     {
-        
-        return $this->render('sgr_calendarios/index.html.twig');
+
+
+        $terminoDefault  = 'Aulas de Docencia';
+
+        return $this->render( 'sgr_calendarios/index.html.twig',
+        	[ 
+        		'sgr_espacios' => $sgrEspacioRepository->findByFilters($sgrTerminoRepository->findOneBy([ 'name' => $terminoDefault ] )),
+        	]
+    	);
     }
 }
