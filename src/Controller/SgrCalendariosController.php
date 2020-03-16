a6ccf4623eaa494ff1b7c2cdf159cd0276a55677
juanantoniofr <juanantonio.fr@gmail.com>
2020-03-16 14:54:04 +0100

    calendarios

--- src/Controller/SgrCalendariosController.php
+++ src/Controller/SgrCalendariosController.php
@@ -26,45 +26,53 @@ class SgrCalendariosController extends AbstractController
     {
 
     	$form = $this->createForm(SgrFiltersSgrEventosType::class);
-      $form->handleRequest($request);
+        $form->handleRequest($request);
 
-      //defaults values
-      $begin = date_create_from_format('d/m/Y H:i', '01/09/2019 00:00', new \DateTimeZone('Europe/Madrid'));
-      $end = date_create_from_format('d/m/Y H:i', '31/08/2020 00:00', new \DateTimeZone('Europe/Madrid'));
-      $termino  =  2;// id de 'Aula de Docencia';
+        //defaults values
+        $aCalendarios = new ArrayCollection();
+        $begin = date_create_from_format('d/m/Y H:i', '01/09/2019 00:00', new \DateTimeZone('Europe/Madrid'));
+        $end = date_create_from_format('d/m/Y H:i', '31/08/2020 00:00', new \DateTimeZone('Europe/Madrid'));
+        $termino  =  2;// id de 'Aula de Docencia';
+        //dump($form->isSubmitted());
+        //dump($form->isValid());
+        //dump($form->getErrors());
 
+        //exit;
+        if ($form->isSubmitted() && $form->isValid()) {
 
-      $data = $form->getData();
-      if ($data['f_inicio'])
-        $begin = date_create_from_format('d/m/Y H:i', $data['f_inicio'] . '00:00', new \DateTimeZone('Europe/Madrid'));
-      if($data['f_fin'])
-        $end = date_create_from_format('d/m/Y H:i', $data['f_fin'] . '00:00', new \DateTimeZone('Europe/Madrid'));
-      //dump($begin);
-      //dump($end);
-      $sgrFechasEvento = $sgrFechasEventoRepository->findBetween($begin, $end);
-      //dump($sgrFechasEvento);
-      //Search by termino
-	    $sgrEspacios = $sgrEspacioRepository->findByFilters($termino);
-      foreach ($sgrEspacios as $sgrEspacio){ 
-        	
-     		$calendario = new Calendario;
-     		$calendario->setSgrEspacio($sgrEspacio);
-     		
-     		$eventos = new ArrayCollection();
-		    foreach ($sgrFechasEvento as $sgrFechaEvento)
-        {
-     			
-     			if ($sgrFechaEvento->getEvento()->getEspacio() == $sgrEspacio)
-     				$calendario->setPeriodsByDay( $sgrFechaEvento->getEvento(), $sgrFechaEvento);
-     		}
-     		
-     		$aCalendarios[] = $calendario;
-     	}
-
-     	//dump($begin);
-      //dump($aCalendarios);
-      //exit;
-   	  return $this->render( 'sgr_calendarios/index.html.twig',[ 
+            $data = $form->getData();
+            
+            if ($data['f_inicio'])
+                $begin = date_create_from_format('d/m/Y H:i', $data['f_inicio'] . '00:00', new \DateTimeZone('Europe/Madrid'));
+            if($data['f_fin'])
+                $end = date_create_from_format('d/m/Y H:i', $data['f_fin'] . '00:00', new \DateTimeZone('Europe/Madrid'));
+            if($data['termino'])
+                $termino = $data['termino'];
+            //dump($termino);
+            //exit;
+            $sgrFechasEvento = $sgrFechasEventoRepository->findBetween($begin, $end);
+            $sgrEspacios = $sgrEspacioRepository->findByFilters($termino);
+            //dump($end->format('Y-m-d'));
+            //exit;
+            
+            foreach ($sgrEspacios as $sgrEspacio){ 
+            	
+         		$calendario = new Calendario;
+         		$calendario->setSgrEspacio($sgrEspacio);
+         		
+         		$eventos = new ArrayCollection();
+    		    foreach ($sgrFechasEvento as $sgrFechaEvento)
+                {
+         			
+         			if ($sgrFechaEvento->getEvento()->getEspacio() == $sgrEspacio)
+         				$calendario->setPeriodsByDay( $sgrFechaEvento->getEvento(), $sgrFechaEvento);
+         		}
+         		
+         		$aCalendarios[] = $calendario;
+         	}
+        }
+     	
+        return $this->render( 'sgr_calendarios/index.html.twig',[ 
       		'aCalendarios' => $aCalendarios,
       		'numDaysView' => (int) $begin->diff($end)->format('%d'),
       		'form'       => $form->createView(),
